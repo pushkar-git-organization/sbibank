d516cdf971516c41f99ec0db0bc255e3f4a2e6dd
Pushkar15062007 <121386420+Pushkar15062007@users.noreply.github.com>
2022-12-27 14:03:16 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,3 +1,4 @@
 This file again created in master branch.
 sadkdvmc cjniwrsndjnqipenfadv jkdgnie;agdnb jzdb wj;eb dlj jegwsndvljmb nsknvkzrngzrgnfkgfsngfjm sfjmfknfngkjgnfs 
 Testing started for git pull, git fetch and git merge.
+Git pull test done in 1st account and started in 2nd account.
